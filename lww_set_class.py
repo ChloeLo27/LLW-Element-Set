d55ed0cd96b75bb1d337428430bcfa3fa076deef
Chloe Lo <chloewing.lo1127@gmail.com>
2019-04-12 17:30:38 +0800

    unit tests and time machine
    - added unit tests for basic methods
    - implemented time machine capabilities
    - added unit tests for time machine
    - no merge cases implemented and tested yet

--- lww_set_class.py
+++ lww_set_class.py
@@ -1,6 +1,7 @@
 """ IMPORT LIBRARIES """
 from datetime import datetime
 from typing import List, TypeVar
+from collections import Hashable
 
 LWW_Set = TypeVar('LWW_Set')
 
@@ -16,9 +17,20 @@ class _LWW_Set__LWW_Element(object):
 	def update_timestamp(self):
 		self.__timestamps.append(datetime.now())
 
+	def last_timestamp_before(self, timestamp):
+		timestamps_before = [t for t in self.__timestamps if t <= timestamp]
+		if timestamps_before:
+			return max(timestamps_before)
+		else:
+			return None
+
 	@property
 	def last_timestamp(self):
 		return max(self.__timestamps)
+
+	@property
+	def first_timestamp(self):
+		return min(self.__timestamps)
 	
 		
 
@@ -34,28 +46,42 @@ class LWW_Set():
 
 	def exist(self, element, timestamp=None):
 		""" check whether an element exists in the LWW set of not """
-		element_in_add_set = [x for x in self.__add_set if x.element == element]
-		element_in_remove_set = [x for x in self.__remove_set if x.element == element]
+
+		if timestamp is None:
+			element_in_add_set = [x for x in self.__add_set if x.element == element]
+			element_in_remove_set = [x for x in self.__remove_set if x.element == element]
+		else:
+			element_in_add_set = [x for x in self.__add_set if x.element == element and x.last_timestamp_before(timestamp) is not None]
+			element_in_remove_set = [x for x in self.__remove_set if x.element == element and x.last_timestamp_before(timestamp) is not None]
+
 		if not element_in_add_set:
 			return False
 		if not element_in_remove_set:
 			return True
 		if timestamp is None:
-			return element_in_remove_set[0].last_timestamp <= element_in_add_set[0].last_timestamp
-		# TODO: look up the existence of the element before the given timestamp
+			return element_in_remove_set[0].last_timestamp < element_in_add_set[0].last_timestamp
 
 	def get(self, timestamp=None) -> List[__LWW_Element]:
 		"""
 		return a list contianing the elements which should be in the set right before the given timestamp;
 		return the most updated state if no timestamp given
 		"""
-		if timestamp is None:
-			return [x.element for x in self.__add_set if self.exist(x.element)]
-		# TODO: return the set at the state right before the given timestamp
+		return set([x.element for x in self.__add_set if self.exist(x.element, timestamp)])
 		
 	def add(self, element) -> LWW_Set:
-		""" add element to the set """
-		if not self.exist(element):
+		"""
+		add element to the set
+
+		NOTE
+		====
+		Cannot accept unhashable types
+		"""
+
+		if not isinstance(element, Hashable):
+			raise TypeError("LWW element set accepts only hashable types.")
+
+		element_in_add_set = [x for x in self.__add_set if x.element == element]
+		if not element_in_add_set:
 			self.__add_set.append(__LWW_Element(element))
 		else:
 			[x for x in self.__add_set if x.element == element][0].update_timestamp()
--- lww_set_test.py
+++ lww_set_test.py
@@ -1,14 +1,104 @@
 """ IMPORT LIBRARIES """
+import unittest
+from datetime import datetime
 from lww_set_class import LWW_Set
 
-new_set = LWW_Set()
-new_set.add("test")
-print(new_set.get())
-new_set.add("another")
-print(new_set.get())
-new_set.add("test")
-print(new_set.get())
-new_set.remove("another")
-print(new_set.get())
-new_set.remove("test")
-print(new_set.get())
+class TestLWWSetBasicMethods(unittest.TestCase):
+
+	def setUp(self):
+		self.new_set = LWW_Set()
+		self.new_set.add("test")
+
+	def test_add_single(self):
+		self.assertEqual(self.new_set.get(), set(["test"]))
+		self.assertEqual(self.new_set.exist("test"), True)
+		self.assertEqual(self.new_set.exist(34), False)
+
+	def test_add_multiple(self):
+		self.new_set.add("another")
+		self.new_set.add(34)
+		self.new_set.add("another")
+		self.assertEqual(self.new_set.get(), set(["test", "another", 34]))
+		self.assertEqual(self.new_set.exist("test"), True)
+		self.assertEqual(self.new_set.exist(34), True)
+
+	def test_hashable(self):
+		with self.assertRaises(TypeError):
+			self.new_set.add(["item"])
+
+	def test_remove_single(self):
+		self.new_set.add("another")
+		self.new_set.remove("test")
+		self.assertEqual(self.new_set.get(), set(["another"]))
+
+	def test_remove_multiple(self):
+		self.new_set.add("another")
+		self.new_set.add(34)
+		self.new_set.add(12345)
+		self.new_set.remove(34)
+		self.new_set.remove(12345)
+		self.assertEqual(self.new_set.get(), set(["test", "another"]))
+		self.assertEqual(self.new_set.exist(34), False)
+
+	def test_remove_nonexistent(self):
+		self.new_set.add("another")
+		self.new_set.add(34)
+		self.new_set.add(12345)
+		self.new_set.remove(345)
+		self.new_set.remove(123)
+		self.assertEqual(self.new_set.get(), set(["test", "another", 34, 12345]))
+
+	def test_remove_and_add(self):
+		self.new_set.add("another")
+		self.new_set.add(34)
+		self.new_set.add(12345)
+		self.new_set.remove(34)
+		self.new_set.remove(12345)
+		self.new_set.add(34)
+		self.assertEqual(self.new_set.get(), set(["test", "another", 34]))
+
+
+class TestTimeMachineMethods(unittest.TestCase):
+
+	def setUp(self):
+		self.new_set = LWW_Set()
+
+	def test_existence_in_given_time(self):
+		self.new_set.add("a")
+		self.new_set.add("b")
+		self.new_set.add("c")
+		retrieval_time = datetime.now()
+		self.new_set.add("d")
+		self.new_set.add("e")
+		self.new_set.add("f")
+		self.assertEqual(self.new_set.exist("a", retrieval_time), True)
+		self.assertEqual(self.new_set.exist("b", retrieval_time), True)
+		self.assertEqual(self.new_set.exist("c", retrieval_time), True)
+		self.assertEqual(self.new_set.exist("d", retrieval_time), False)
+		self.assertEqual(self.new_set.exist("e", retrieval_time), False)
+		self.assertEqual(self.new_set.exist("f", retrieval_time), False)
+
+	def test_retrieval_in_given_time(self):
+		self.new_set.add("a")
+		self.new_set.add("b")
+		self.new_set.add("c")
+		retrieval_time = datetime.now()
+		self.new_set.add("d")
+		self.new_set.add("e")
+		self.new_set.add("f")
+		self.assertEqual(self.new_set.get(retrieval_time), set(["a", "b", "c"]))
+
+
+
+# TODO: merge behavkour
+# class TestLWWSetMergeBehaviour(unittest.TestCase):
+
+# 	def setUp(self):
+# 		self.set_1 = LWW_Set()
+# 		self.set_2 = LWW_Set()
+
+# 	def test_something(self):
+# 		self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+	unittest.main(verbosity=2)
